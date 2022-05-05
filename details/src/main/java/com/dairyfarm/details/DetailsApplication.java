d23158b08eb6cb02b97cd3da000a98a48c253ae5
anil1807 <anilkumarreddyns1807@gmail.com>
2022-05-05 23:55:44 +0530

    html1

--- details/src/main/java/com/dairyfarm/details/DetailsApplication.java
+++ details/src/main/java/com/dairyfarm/details/DetailsApplication.java
@@ -20,7 +20,7 @@ public class DetailsApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:63342/details/templates"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
